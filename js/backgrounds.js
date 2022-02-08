1cede2c8124dc29a511ea039205814e5f52289b5
jwh926 <tridath3@gmail.com>
2022-02-08 20:02:08 +0900

    Add some folders

--- js/backgrounds.js
+++
@@ -1,8 +0,0 @@
-const images = ["0.jpg", "1.jpg", "2.jpeg", "3.jpeg"];
-
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-const bgImage = document.createElement("img");
-bgImage.src = `img/${chosenImage}`;
-bgImage.setAttribute("class", "bg");
-document.body.prepend(bgImage);
